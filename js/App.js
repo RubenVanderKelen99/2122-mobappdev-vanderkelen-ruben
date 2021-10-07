68c43613ee4bf48357f10cf82a557db2bbf1c770
Ruben <Ruben@LAPTOP-VU6RSGG2>
2021-10-07 14:56:42 +0200

    Verbinding maken met backend (API van Back4App) - werkt niet

--- js/App.js
+++ js/App.js
@@ -1,15 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+// In a React Native application
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+//Initializing the SDK
+Parse.setAsyncStorage(AsyncStorage);
+//Paste below the Back4App Application ID AND the JavaScript KEY
+Parse.initialize('ubXN5OEnvNKWCrDHIx20SpeWrHkWizuB4Ue3cyoy', '92WYjKgyN8VScCjQtX0GEbqp4doU6kqpj0rrGHDT');
+//Point to Back4App Parse API address
+Parse.serverURL = 'https://parseapi.back4app.com/parse';
+
+const App = () => {
+    const [user, setUser] = useState(new Parse.Object('User'));
+
+   async function addUser() {
+        try {
+            //create a new Parse Object instance
+            const newUser = new Parse.Object('User');
+            //define the attributes you want for your Object
+            newUser.set('username', 'Mark.Doe');
+            newUser.set('name', 'Mark');
+            newUser.set('surName', 'Doe');
+            newUser.set('password', 'Azerty123');
+            newUser.set('phone', '111222333');
+            newUser.set('countryCode', 'BE');
+            newUser.set('email', 'MarkDoe@odisee.be');
+            console.log(newUser.get('username'));
+            console.log(newUser.get('name'));
+            console.log(newUser.get('surName'));
+            console.log(newUser.get('password'));
+            console.log(newUser.get('phone'));
+            console.log(newUser.get('countryCode'));
+            console.log(newUser.get('email'));
+            //save it on Back4App Data Store
+            await newUser.save();
+            console.log('Status', 'Succes');
+        } catch (error) {
+            console.log('Error saving new user: ', error);
+        }
+    }
+
+    async function fetchUser() {
+        //create your Parse Query using the Person Class you've created
+        let query = new Parse.Query('User');
+        console.log(query);
+        //run the query to retrieve all objects on Person class, optionally you can add your filters
+        let queryResult = await query.find();
+        console.log(query);
+        //the resul is an arry of objects. Pick the first result
+        const currentUser = queryResult[0];
+        //access the Parse Object attributes
+        console.log('User id: ', currentUser.get('id'));
+        console.log('User username: ', currentUser.get('username'));
+        console.log('User name: ', currentUser.get('name'));
+        console.log('User surname: ', currentUser.get('surName'));
+        console.log('User phone: ', currentUser.get('phone'));
+        console.log('User countrycode: ', currentUser.get('countryCode'));
+        console.log('User email: ', currentUser.get('email'));
+        setUser(currentUser);
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, []);
+
+    /*async function addPriceClass() {
+            try {
+                //create a new Parse Object instance
+                const newClass = new Parse.Object('PriceClass');
+                //define the attributes you want for your Object
+                newClass.set('minPrice', 10);
+                newClass.set('maxPrice', 20);
+                //save it on Back4App Data Store
+                await newClass.save();
+                console.log('Status', 'Succes');
+            } catch (error) {
+                console.log('Error saving new user: ', error);
+            }
+        }*/
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Name: {user.get('name')}</Text>
+                <Text>email: {user.get('email')}</Text>
+                <Button title='Add person' onPress={addUser} />
+                <Button title='Fetch person' onPress={fetchUser} />
+            </View>
+        </SafeAreaView>
+    );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
--- logboek/logboek.md
+++ logboek/logboek.md
@@ -2,8 +2,27 @@
 ### Taxi app - Ruben Van der Kelen
 
 #### Week 1
-Er werd een prototype in Figma gemaakt die de volgende pagina's bevat: Aanmelden, Verificatie identiteit, Home, Rit Aanmaken (met en zonder tussenstop), Rit bevestigen, Menu (Sidebar), Betalingen, Reisgeschiedenis en Promoties.
+Prototype in Figma gemaakt die de volgende pagina's bevat: Aanmelden, Verificatie identiteit, Home, Rit Aanmaken (met en zonder tussenstop), Rit bevestigen, Menu (Sidebar), Betalingen, Reisgeschiedenis en Promoties.
 
 #### Week 2
-Verder afwerken van het prototype in Figma.
-Inplannen user tests van prototype, research van ReactNative en opzetten file structuur.
+Verder afwerken van het prototype in Figma en inplannen user testen hiervan.
+Database design en uitwerking eerste versie in Back4App.
+Research van ReactNative, NativeBase en installatie project hiervan.
+Leren werken met Android Studio.
+
+#### Week 3
+
+Gedaan
+Problemen
+1. Bij het toevoegen van Back4App is het niet mogelijk om het commando cd ios & pod-install uit te voeren.
+2. Ik kan niet verbinden met de parse-server van Back4App.
+   Ik krijg: Possible Unhandled Promise Rejection (id: 1) en Error: XMLHttpRequest failed: "Unable to connect to the Parse API".
+Opgelost
+1. pod-install is enkel nodig voor het developen in ios en dit laat ik momenteel achterwege.
+2. geprobeerd: nieuwe app aanmaken op Back4App, Parse server downgraden naar vorige versie
+
+Bronnen
+- https://www.back4app.com/docs/react-native/parse-sdk/react-native-sdk
+- https://www.back4app.com/docs/android/login-android-tutorial
+- https://www.npmjs.com/package/@parse/react-native
+- https://github.com/parse-community/parse-server/issues/667
