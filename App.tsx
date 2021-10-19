e51a619cc9b19f53c3043f7db2a684be5e4b06ff
Ruben <Ruben@LAPTOP-VU6RSGG2>
2021-10-19 09:58:22 +0200

    Verbinding maken met firestore database

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { db } from './js/firebase';
 
 export default function App() {
+    const [test, setTest] = useState([]);
+    useEffect(() => {
+        const ref = db.collection('test');    ref.onSnapshot((query) => {
+            const objs = [];
+            query.forEach((doc) => {
+              objs.push({
+                id: doc.id,
+                ...doc.data(),
+              });
+            });
+            setTest(objs);
+          });
+    }, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+     <View style={styles.container}>
+          {test.map((obj) => (
+            <View id={obj.id}>
+              <Text>{obj.name}</Text>
+            </View>  ))}
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
 
 const styles = StyleSheet.create({
   container: {
