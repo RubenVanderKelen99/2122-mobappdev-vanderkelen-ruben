f920edd01de9ca0f2b80b8c78df04ee43c904a7d
Ruben <Ruben@LAPTOP-VU6RSGG2>
2021-10-19 09:41:43 +0200

    Aanmaak nieuw expo project

--- js/screens/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LoginScreen } from './Login/LoginScreen'
-export { default as HomeScreen } from './Home/HomeScreen'
-export { default as RegistrationScreen } from './Registration/RegistrationScreen'
