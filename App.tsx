5f4683df39a70daf60fb4a01c90ae61e708713b2
ruben.vanderkelen <ruben.vanderkelen@student.odisee.be>
2021-11-03 13:44:45 +0100

    Stack Navigator Setup fix (import react-native-gesture-handler + install @react-native-masked-view/masked-view)

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
